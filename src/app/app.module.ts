e9abb6d9ab86fc36ac845e00ce60019e036d41d9
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-22 14:36:10 +0300

    add shopping-edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import {AppComponent} from './app.component';
 import {HeaderComponent} from "./header/header.component";
 import {RecipesComponent} from './recipes/recipes.component';
 import {RecipesListComponent} from './recipes/recipes-list/recipes-list.component';
-import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import {RecipesItemComponent} from "./recipes/recipes-list/recipes-item/recipes-item.component";
+import {RecipesDetailComponent} from './recipes/recipes-detail/recipes-detail.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 
-// @ts-ignore
-// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent,
     RecipesDetailComponent,
+    RecipesListComponent,
+    RecipesItemComponent,
+    ShoppingListComponent,
     ShoppingEditComponent,
   ],
   imports: [
@@ -25,4 +27,5 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
