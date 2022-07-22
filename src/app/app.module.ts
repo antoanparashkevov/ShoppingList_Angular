769dc31b40bb7ebc3bedb0f289ce9559609cc9ff
“antoanparashkevov” <tonkata1505@gmail.com>
2022-07-22 14:29:42 +0300

    add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipesListComponent} from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
+// @ts-ignore
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule
@@ -13,4 +25,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
